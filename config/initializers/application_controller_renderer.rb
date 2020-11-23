2421176bc3a77c8bb39e1f32a5c3b92c0fb180e8
Ami-tnk <ami1y251992@gmail.com>
2020-11-23 14:15:19 +0000

    [Finish]4-3 画像投稿機能の作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'edcc880dcb28f0a34f20e87da4a1e7f6239f0d65816075aca1c17b65642f53c0fd6a6d5bfbe32d6de7b746b2ccbb3b64420f7286b5d488f121d6434c38c10d97'
