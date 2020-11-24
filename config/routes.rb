249bc878eae141644c0d5f6106b1d7aaaa343f39
Ami-tnk <ami1y251992@gmail.com>
2020-11-24 02:16:18 +0000

    [Start]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
